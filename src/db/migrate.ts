22a1c75dfa1ac6978238a32b690a1ca8a55d2df6
Mario Lazzari <mario.lazzari@gmail.com>
2024-11-02 22:28:34 +0100

    db migrate

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -3,7 +3,7 @@ import { migrate } from "drizzle-orm/neon-http/migrator";
 
 const main = async () => {
   try {
-    await migrate(db, { migrationsFolder: "src/db/mirations" });
+    await migrate(db, { migrationsFolder: "src/db/migrations" });
     console.log("Migration completed");
   } catch (ex) {
     console.error("Migration error", ex);
